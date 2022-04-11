2949d19700b48974f0bd0ea654e0929a69f3011e
Put <1012819761@qq.com>
2022-04-11 20:53:59 +0800

    fix: 目前最能用的

--- src/App.js
+++ src/App.js
@@ -1,100 +1,150 @@
 import React, { useState } from "react";
 import { Table } from "antd";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import "./index.css"
+import "./index.css";
 
 export default function App() {
   const columns = [
     {
       title: "Name",
       dataIndex: "name",
-      className: "drag-visible"
+      className: "ant-table-cell",
     },
     {
       title: "Age",
-      dataIndex: "age"
+      dataIndex: "age",
+      className: "ant-table-cell",
     },
     {
+      title: "Age",
+      dataIndex: "age",
+      className: "ant-table-cell",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      className: "ant-table-cell",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      className: "ant-table-cell",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      className: "ant-table-cell",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      className: "ant-table-cell",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      className: "ant-table-cell",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      className: "ant-table-cell",
+    },
+
+    {
       title: "Address",
-      dataIndex: "address"
-    }
+      dataIndex: "address",
+      fixed: "right",
+    },
   ];
 
   const [dataSource, setDataSource] = useState([
     {
       key: "1",
       name: "11111",
-      age: 32,
-      address: "New York No. 1 Lake Park"
+      age: "asjdljasldjlasjldkjlaksjdlkjaslkdjklasjdlasjdljasldjlasjldkjlaksjdlkjaslkdjklasjdlasjdljasldjlasjldkjlaksjdlkjaslkdjklasjdlasjdljasldjlasjldkjlaksjdlkjaslkdjklasjdl",
+      address: "New York No. 1 Lake Park",
+      className: "ant-table-cell",
     },
     {
       key: "22222",
       name: "Jim Green",
       age: 42,
-      address: "London No. 1 Lake Park"
+      address: "London No. 1 Lake Park",
+      className: "ant-table-cell",
     },
     {
       key: "33333",
       name: "Joe Black",
       age: 39,
-      address: "Sidney No. 1 Lake Park"
+      address: "Sidney No. 1 Lake Park",
+      className: "ant-table-cell",
     },
     {
       key: "44444",
       name: "John Brown",
       age: 32,
-      address: "New York No. 1 Lake Park"
+      address: "New York No. 1 Lake Park",
+      className: "ant-table-cell",
     },
     {
       key: "55555",
       name: "55555",
       age: 42,
-      address: "London No. 1 Lake Park"
+      address: "London No. 1 Lake Park",
+      className: "ant-table-cell",
     },
     {
       key: "66666",
       name: "66666",
       age: 39,
-      address: "Sidney No. 1 Lake Park"
+      address: "Sidney No. 1 Lake Park",
+      className: "ant-table-cell",
     },
     {
       key: "7",
       name: "7777",
       age: 42,
-      address: "London No. 1 Lake Park"
+      address: "London No. 1 Lake Park",
     },
     {
       key: "8",
       name: "8888",
       age: 39,
-      address: "Sidney No. 1 Lake Park"
+      address: "Sidney No. 1 Lake Park",
     },
-    
   ]);
   const DragableBodyRow = (props) => {
-    console.log(props);
+    console.log("props---");
     const index = dataSource.findIndex(
       (row) => row.key === props["data-row-key"]
     );
-    return (
+    return props.children.length === 10 ? (
       <Draggable
         key={props["data-row-key"]}
         draggableId={props["data-row-key"].toString()}
         index={index}
+        data-row-key={props["data-row-key"]}
       >
         {(provided, snapshot) => {
           return (
             <tr
-              {...props}
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
-              ref={provided.innerRef}
-              className={snapshot.isDragging ? "hello" : props.className}
-            />
+            {...props}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            className={
+              snapshot.isDragging
+                ? `${props.className} hello`
+                : props.className
+            }
+            >
+              
+            </tr>
           );
         }}
-      </Draggable>
-    );
+      </Draggable> 
+    ) : <tr {...props}></tr>;
   };
   const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
@@ -103,7 +153,7 @@ export default function App() {
 
     return result;
   };
-  
+
   const onDragEnd = (result) => {
     console.log(result);
     if (!result.destination) {
@@ -129,7 +179,6 @@ export default function App() {
                 {...provided.droppableProps}
                 ref={provided.innerRef}
               ></tbody>
-              {provided.placeholder}
             </>
           );
         }}
@@ -139,8 +188,8 @@ export default function App() {
   const components = {
     body: {
       wrapper: draggableBody,
-      row: DragableBodyRow
-    }
+      row: DragableBodyRow,
+    },
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -150,6 +199,7 @@ export default function App() {
         columns={columns}
         rowKey="key"
         components={components}
+        scroll={{ x: 1480 }}
       />
     </DragDropContext>
   );
